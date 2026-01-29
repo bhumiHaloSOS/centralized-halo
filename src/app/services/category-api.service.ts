c2efad9f3eb3126d5ab933dfb28acddc9903bc3d
Bhumi <bhumi@halosos.com>
2026-01-29 08:47:47 -0500

    Fix BE error

--- src/app/services/category-api.service.ts
+++ src/app/services/category-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 // Define the interface inside the service
 export interface CategoryEntry {
